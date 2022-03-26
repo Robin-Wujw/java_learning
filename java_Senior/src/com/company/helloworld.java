bf3b98f25f4d41c450487e6dfd056774db3e9959
Robin_Wujw <robin.wujw@gmail.com>
2022-03-26 20:45:07 +0800

    更新, 提交人: Robin, 提交时间: 2022-03-26 20:45:00

---
+++ java_Senior/src/com/company/helloworld.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * @Author: Robin_Wujw
+ * @Date: 2022/3/24 19:13
+ */
+public class helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("helloworld");
+    }
+}
