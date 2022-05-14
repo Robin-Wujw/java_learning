1a8e1438eac80bd0a65bfd852df793aa2a20bef5
Robin_Wujw <robin.wujw@gmail.com>
2022-05-14 19:19:13 +0800

    更新, 提交人: Robin, 提交时间: 2022-05-14 19:19:10

---
+++ java_Senior/src/day05/chat01/MultiClient.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * 在线聊天室：客户端
+ * 目标：实现一个客户可以正常收发多条信息
+ * @Author: Robin_Wujw
+ * @Date: 2022-05-03 14:50
+ */
+public class MultiClient {
+    public static void main(String[] args) throws IOException {
+        System.out.println("------client--------");
+        //1.建立连接 使用Socket创建客户端 + 服务器的地址和端口
+        Socket client = new Socket("localhost",8888);
+        //2.客户端发送消息
+        //2.操作： 输入输出流操作
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+        DataInputStream dis = new DataInputStream(client.getInputStream());
+        boolean isRunning = true;
+        while(isRunning) {
+            String msg = console.readLine();
+            dos.writeUTF(msg);
+            dos.flush();
+            //3.获取消息
+            msg = dis.readUTF();
+            System.out.println(msg);
+        }
+        dos.close();
+        dis.close();
+        client.close();
+    }
+}
